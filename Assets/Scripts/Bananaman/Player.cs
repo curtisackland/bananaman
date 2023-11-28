bd4182ec14ea172747949cbbc59184fab9709703
Curtman <curtisackland@rogers.com>
2023-11-28 16:35:04 -0500

    Started player hud and player script

---
+++ Assets/Scripts/Bananaman/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxLaserTime = 100;
+    public int currentLasterTime = 100;
+    public float maxLaserCooldown = 2;
+    public float currentLaserCooldown = 0;
+    public int health = 100;
+    public int score = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
